277c62847e05bc4c57661da71183d8775bab675b
Keaton Freed <keatonafreed2@icloud.com>
2024-01-27 20:23:27 -0800

    Fix v1 compile

--- src/pages/Display.jsx
+++ src/pages/Display.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { db } from '../firebase';
+import { db } from '../firebase.js';
 
 import DisplayRow from '../components/DisplayRow'
 import uploadIcon from "../content/uploadIcon.png"
